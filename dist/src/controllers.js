e4c1720f6c44629665dd578c0ee7f3c1f8c9060b
Onur Satici <onursatici@gmail.com>
2015-04-22 18:21:33 +0300

    add colors array

--- dist/src/controllers.js
+++ dist/src/controllers.js
@@ -1,19 +1,14 @@
 angular.module('karolaj')
     .controller('PatternController', function ($scope) {
       $scope.colors=[];
-      $scope.show = function(){
-        $scope.display = true;
-      };
-      $scope.remove = function(color){
-        if(color) console.log(color);
-        //TODO implement the removal of color from colors
-       $scope.display = false;
+      $scope.remove = function(idx){
+        console.log($scope.colors[idx]);
+        $scope.colors.splice(idx,1)
       };
       $scope.add = function(){
-        console.log($scope.newColor);
-        $scope.colors.push($scope.newColor);
+        $scope.colors.push("#aaaaaa");
         console.log($scope.colors);
-        $scope.remove();
+        //$scope.remove();
       };
     });
 
